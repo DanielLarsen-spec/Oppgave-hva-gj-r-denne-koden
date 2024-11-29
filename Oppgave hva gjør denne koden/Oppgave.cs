004f52e41731c23e42524cb8383de48ab0e49f38
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-11-29 19:10:58 +0100

    Add project files.

---
+++ Oppgave hva gjør denne koden/Oppgave.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oppgave_hva_gjør_denne_koden
+{
+    internal class Oppgave
+    {
+        public void Run()
+        {
+            var range = 250;
+            var counts = new int[range];
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine();
+                foreach (var character in text ?? string.Empty)
+                {
+                    counts[(int)character]++;
+                }
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        Console.WriteLine(character + " - " + counts[i]);
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Oppgave hva gjør denne koden/Program.cs
@@ -0,0 +1,4 @@
+﻿using Oppgave_hva_gjør_denne_koden;
+
+var oppgave = new Oppgave();
+oppgave.Run();
